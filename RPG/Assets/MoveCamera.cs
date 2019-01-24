01dfdcca4e1b6f875015e928407aa85aa90c804e
William <william.wirsen@live.se>
2019-01-24 19:32:37 +0100

    Created the ability to move the camera
    Created the ability to move the camera with right mouse click

---
+++ RPG/Assets/MoveCamera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class MoveCamera : MonoBehaviour
+    {
+        FreeLookCam cam;
+        // Start is called before the first frame update
+        void Start()
+        {
+            cam = GetComponent<FreeLookCam>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleUserInputs();
+        }
+
+        private void HandleUserInputs()
+        {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                cam.enabled = true;
+            }
+            if (Input.GetButtonUp("Fire2"))
+            {
+                cam.enabled = false;
+            }
+        }
+    }
+}
+
