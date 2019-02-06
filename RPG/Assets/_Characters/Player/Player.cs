5ee96cee0d5bd6b5b1a199c778122672ead5a6c2
William <william.wirsen@live.se>
2019-02-06 22:58:47 +0100

    Updated instantiation of arrow

--- RPG/Assets/_Characters/Player/Player.cs
+++ RPG/Assets/_Characters/Player/Player.cs
@@ -27,6 +27,7 @@ namespace RPG.Characters
         GameObject projectileModel;
         GameObject dominantHand;
         GameObject arrowHand;
+        GameObject playerWeapon;
         float currentHealth;
         float currentStamina;
         float timeBetweenAttacks;
@@ -56,7 +57,7 @@ namespace RPG.Characters
             attackTimer = 0f;
         }
 
-        
+
 
         // Update is called once per frame
         void Update()
@@ -64,8 +65,8 @@ namespace RPG.Characters
             if (isAlive)
             {
                 HandleUserInput();
-                if(!staminaCD)
-                    HandleStaminaRecovery();                
+                if (!staminaCD)
+                    HandleStaminaRecovery();
             }
 
         }
@@ -82,44 +83,61 @@ namespace RPG.Characters
         {
             staminaCD = true;
             yield return new WaitForSeconds(cooldown);
-            if(currentStamina < maxStamina)
+            if (currentStamina < maxStamina)
             {
                 currentStamina = currentStamina + staminaRecoveryRate;
                 Debug.Log("Added stamina");
-            }                
+            }
             staminaCD = false;
-            
-            
+
+
         }
 
         private void SetupWeapon()
         {
+
+
             weaponPrefab = weaponInUse.GetWeaponModel;
             type = weaponInUse.type;
             weapon = weaponPrefab.GetComponent<Weapon>();
             timeBetweenAttacks = weaponInUse.GetTimeBetweenAttacks;
-            staminaDrain = weaponInUse.GetStaminaDrain; 
+            staminaDrain = weaponInUse.GetStaminaDrain;
         }
 
         private void PutWeaponInHands()
         {
             weaponPrefab = weaponInUse.GetWeaponModel;
             if (weaponInUse != null && weaponPrefab != null)
-            {                
+            {
                 dominantHand = RequestDominantHand();
                 var weapon = Instantiate(weaponPrefab, dominantHand.transform);
                 weapon.transform.localPosition = weaponInUse.weaponGrip.localPosition;
                 weapon.transform.localRotation = weaponInUse.weaponGrip.localRotation;
 
                 // TODO: Change to bow only
-                if(type == WeaponType.Ranged)
+                if (type == WeaponType.Ranged)
                 {
                     rangedWeapon = true;
-                    //GameObject arrowSpawnPoint = weaponInUse.GetProjectilePrefab.transform.Find("ArrowSpawnPoint").gameObject;
-                    //Instantiate(weaponInUse.GetProjectilePrefab, arrowSpawnPoint.transform.position, Quaternion.identity);
+
+                    playerWeapon = GameObject.FindGameObjectWithTag("Weapon");
+                    GameObject bowRoot;
+                    GameObject stringSpawn;
+
+                    bowRoot = playerWeapon.transform.Find("Bow_Root").gameObject;
+                    bowRoot = bowRoot.transform.Find("Bow_Jnt").gameObject;
+                    if (bowRoot != null)
+                    {
+                        stringSpawn = bowRoot.transform.Find("String_jnt").gameObject;
+                        Debug.Log(stringSpawn);
+                        var projectile = Instantiate(weaponInUse.GetProjectilePrefab, stringSpawn.transform);
+                        projectile.transform.localPosition = stringSpawn.transform.localPosition + new Vector3(-0.25f, 0,0);
+                        projectile.transform.localRotation = Quaternion.Euler(0f, -90f, 90f);
+                    }
                 }
+
                 
-            }        
+
+            }
         }
 
         private GameObject RequestDominantHand()
@@ -128,7 +146,7 @@ namespace RPG.Characters
             int numberOfDominantHands = dominantHands.Length;
             Assert.IsFalse(numberOfDominantHands <= 0, "Could not find any dominant hands, please add one");
             Assert.IsFalse(numberOfDominantHands > 1, "Found multiple dominant hands, please remove one");
-            return dominantHands[0].gameObject; 
+            return dominantHands[0].gameObject;
         }
 
         private void SetTestResources()
@@ -141,7 +159,7 @@ namespace RPG.Characters
         {
             isAlive = true;
             currentHealth = maxHealth;
-            currentStamina = maxStamina; 
+            currentStamina = maxStamina;
         }
 
         public float healthAsPercentage
@@ -172,7 +190,7 @@ namespace RPG.Characters
         }
 
         private void HandleUserInput()
-        {            
+        {
             if (Input.GetButtonDown("Fire1"))
             {
                 if (currentStamina >= staminaDrain)
@@ -184,26 +202,27 @@ namespace RPG.Characters
 
         private void AttackTarget()
         {
-            if(isAlive)
+            if (isAlive)
             {
-                if(currentStamina >= staminaDrain)
+                if (currentStamina >= staminaDrain)
                 {
                     if (Time.time - attackTimer > timeBetweenAttacks)
                     {
                         currentStamina = currentStamina - staminaDrain;
                         animator.SetTrigger("Attack");
 
-                        if(rangedWeapon)
+                        if (rangedWeapon)
                         {
-                            GameObject playerWeapon = GameObject.FindGameObjectWithTag("Weapon");
                             playerWeapon.GetComponent<Animator>().SetTrigger("Shoot");
+
+
                         }
 
                         attackTimer = Time.time;
                     }
                 }
-                
-            }            
+
+            }
         }
 
         public WeaponConfig GetPlayerWeapon
@@ -218,29 +237,29 @@ namespace RPG.Characters
         {
             if (godMode)
                 return;
-            if(isAlive)
+            if (isAlive)
             {
-                if(currentHealth > 0 && currentHealth <= maxHealth)
+                if (currentHealth > 0 && currentHealth <= maxHealth)
                 {
                     currentHealth = currentHealth - damage;
-                    lastHitTimer = Time.time; 
+                    lastHitTimer = Time.time;
                 }
 
                 if (currentHealth >= maxHealth)
-                    currentHealth = maxHealth;                    
+                    currentHealth = maxHealth;
 
                 //Debug.Log("Player current health:" + currentHealth); 
                 if (currentHealth <= 0)
                 {
                     Die();
-                    currentHealth = 0; 
+                    currentHealth = 0;
                 }
             }
         }
 
         private void Die()
         {
-            Debug.Log("Player died"); 
+            Debug.Log("Player died");
             animator.enabled = false;
             thirdPersonUserControl.enabled = false;
             isAlive = false;
